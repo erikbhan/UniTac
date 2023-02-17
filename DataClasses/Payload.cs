8430303998a23f84c63a1e25dc5ac4db0b9984bf
Gilgard <Hjelljord.alida@gmail.com>
2023-02-17 10:18:42 +0100

    JSON klasser lagt til

---
+++ DataClasses/Payload.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class Payload
+{
+    // {"messageType":"position",
+    // "id":"974682A",
+    // "collector_id":"TAC-B",
+    // "collector_serial":"051001572",
+    // "total_detected_objects":1,
+    // "object_list":{
+        // "object0":{
+            // "ID":76,
+            // "speed":0.0,
+            // "X":[-75,-93,0,0,0,0,0,0,0,0],
+            // "Y":[40,51,0,0,0,0,0,0,0,0]
+            // }
+        // }
+    // }
+    public string messageType { get; set; } = string.Empty;
+    public string id { get; set; } = string.Empty;
+    public string collector_id { get; set; } = string.Empty;
+    public string collector_serial { get; set; } = string.Empty;
+    public int total_detected_objects { get; set; }
+    public Dictionary<string,ObjectPosition> object_list { get; set; } = new Dictionary<string,ObjectPosition>();
+}
