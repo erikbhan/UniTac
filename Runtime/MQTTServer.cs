be4cccddd683599e5f129715e6fd9e99cafa073f
Erik Borgeteien Hansen <38139009+erikbhan@users.noreply.github.com>
2023-02-17 13:54:36 +0100

    change server start to a Unity coroutine

--- Runtime/MQTTServer.cs
+++ Runtime/MQTTServer.cs
@@ -2,42 +2,83 @@ using MQTTnet;
 using MQTTnet.Diagnostics;
 using MQTTnet.Server;
 using System;
-using System.Threading.Tasks;
+using System.Collections;
 using UnityEngine;
 
 public class MQTTServer : MonoBehaviour
 {
-    private void Start()
+    [SerializeField] bool enableLogging = false;
+    [SerializeField] int port = 1883;
+    [SerializeField] int connectionBacklog = 100;
+
+    Coroutine startServerCoroutine;
+    MqttServer mqttServer;
+
+    public void Start()
     {
-        Run_Server_With_Logging();
+        if (startServerCoroutine == null && mqttServer == null)
+        {
+            startServerCoroutine = StartCoroutine(StartServerAsync());
+        } 
+        else
+        {
+            Debug.LogWarning("Server already starting or started");
+        }
     }
 
-    public async void Run_Server_With_Logging()
+    IEnumerator StartServerAsync()
     {
-        var mqttFactory = new MqttFactory(new ConsoleLogger());
-        var mqttServerOptions = new MqttServerOptionsBuilder().WithDefaultEndpoint().Build();
-        var mqttServer = mqttFactory.CreateMqttServer(mqttServerOptions);
-        await mqttServer.StartAsync();
-        await Task.Yield();
+        var mqttServerOptions = new MqttServerOptionsBuilder()
+            .WithConnectionBacklog(connectionBacklog)
+            .WithDefaultEndpointPort(port)
+            .Build();
+
+        MqttFactory mqttFactory;
+        if (enableLogging)
+        {
+            mqttFactory = new MqttFactory(new ConsoleLogger());
+        }
+        else
+        {
+            mqttFactory = new MqttFactory();
+        }
+        using (mqttServer = mqttFactory.CreateMqttServer(mqttServerOptions))
+        {
+            mqttServer.StartAsync();
+            yield return new WaitUntil(() => mqttServer.IsStarted);
+        }
     }
 
     class ConsoleLogger : IMqttNetLogger
     {
         readonly object _consoleSyncRoot = new();
-
         public bool IsEnabled => true;
 
 #nullable enable
         public void Publish(MqttNetLogLevel logLevel, string source, string message, object[]? parameters, Exception? exception)
+#nullable disable
         {
             if (parameters?.Length > 0) { message = string.Format(message, parameters); }
 
             lock (_consoleSyncRoot)
             {
-                Debug.Log(message);
-                if (exception != null) { Debug.LogError(exception); }
+                switch (logLevel)
+                {
+                    case MqttNetLogLevel.Warning:
+                        Debug.LogWarning(message);
+                        break;
+
+                    case MqttNetLogLevel.Error:
+                        Debug.LogError(message);
+                        break;
+
+                    default:
+                        Debug.Log(message);
+                        break;
+                }
+
+                if (exception != null) { Debug.LogException(exception); }
             }
         }
-#nullable disable
     }
 }
