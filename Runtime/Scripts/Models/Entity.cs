8d649e6f9a612a590263b52b8251898c580943a8
Gilgard <Hjelljord.alida@gmail.com>
2023-04-11 11:16:04 +0200

    Added namespace

--- Runtime/Scripts/Models/Entity.cs
+++ Runtime/Scripts/Models/Entity.cs
@@ -1,88 +1,90 @@
 ﻿using System;
 using Unity.Plastic.Newtonsoft.Json;
 
-/// <summary>
-/// Class for objects detected by the sensor, sensor payload carries <see cref="Dictionary" />&lt;<see cref="string"/>,<see cref="Entity"/>&gt;
-/// </summary>
-[Serializable]
-public class Entity
-{
-    // Json format from sensor:
-    // "{
-    //    "ID":76,
-    //    "speed":0.0,
-    //    "X":[-75,-93,0,0,0,0,0,0,0,0],
-    //    "Y":[40,51,0,0,0,0,0,0,0,0]
-    // }"
-    
+namespace UniTac {
     /// <summary>
-    /// Id given to the entity by the sensor.
+    /// Class for objects detected by the sensor, sensor payload carries <see cref="Dictionary" />&lt;<see cref="string"/>,<see cref="Entity"/>&gt;
     /// </summary>
-    [JsonProperty("ID")]
-    public long Id { get; set; }
-    /// <summary>
-    /// Detected speed of the entity.
-    /// </summary>
-    [JsonProperty("speed")]
-    public float Speed { get; set; }
-    /// <summary>
-    /// Array representing the entity's last 10 X-coordinates. 
-    /// The first being the most resent known position. 
-    /// </summary>
-    public int[] X { get; set; } = new int[10];
-    /// <summary>
-    /// Array representing the entity's last 10 Y-coordinates. 
-    /// The first being the most resent known position. 
-    /// </summary>
-    public int[] Y { get; set; } = new int[10];
-
-    /// <summary>
-    /// Returns a string that represents the current payload.
-    /// </summary>
-    /// <returns>
-    /// <see cref="string" />
-    /// a string that represents the current payload.
-    /// </returns>
-    public override string ToString()
+    [Serializable]
+    public class Entity
     {
-        string s = "Id: " + Id + "\nSpeed: " + Speed + "\n";
-        s += "X: [ ";
-        foreach (var x in X) { s += x + ", "; }
-        s += "]\n";
-        s += "Y: [ ";
-        foreach (var y in Y) { s += y + ", "; }
-        s += "]\n";
-        return s;
-    }
+        // Json format from sensor:
+        // "{
+        //    "ID":76,
+        //    "speed":0.0,
+        //    "X":[-75,-93,0,0,0,0,0,0,0,0],
+        //    "Y":[40,51,0,0,0,0,0,0,0,0]
+        // }"
 
-    /// <summary>
-    /// Calculates the distance from this entity to the origin
-    /// </summary>
-    /// <returns><see cref="double"/> distance from parent sensor</returns>
-    public double DistanceFromParent()
-    {
-        return Math.Sqrt(Math.Pow(this.X[0], 2.0) + Math.Pow(this.Y[0], 2.0));
-    }
+        /// <summary>
+        /// Id given to the entity by the sensor.
+        /// </summary>
+        [JsonProperty("ID")]
+        public long Id { get; set; }
+        /// <summary>
+        /// Detected speed of the entity.
+        /// </summary>
+        [JsonProperty("speed")]
+        public float Speed { get; set; }
+        /// <summary>
+        /// Array representing the entity's last 10 X-coordinates. 
+        /// The first being the most resent known position. 
+        /// </summary>
+        public int[] X { get; set; } = new int[10];
+        /// <summary>
+        /// Array representing the entity's last 10 Y-coordinates. 
+        /// The first being the most resent known position. 
+        /// </summary>
+        public int[] Y { get; set; } = new int[10];
 
-    /// <summary>
-    /// Checks if the given object is equal to this entity
-    /// </summary>
-    /// <param name="obj"><see cref="object"/> object to check</param>
-    /// <returns><see cref="bool"/> true if input is entity with the same id</returns>
-    public override bool Equals(object obj)
-    {
-        if (obj == null) return false;
-        if (obj is not Entity) return false;
-        Entity other = (Entity)obj;
-        return this.Id == other.Id;
-    }
+        /// <summary>
+        /// Returns a string that represents the current payload.
+        /// </summary>
+        /// <returns>
+        /// <see cref="string" />
+        /// a string that represents the current payload.
+        /// </returns>
+        public override string ToString()
+        {
+            string s = "Id: " + Id + "\nSpeed: " + Speed + "\n";
+            s += "X: [ ";
+            foreach (var x in X) { s += x + ", "; }
+            s += "]\n";
+            s += "Y: [ ";
+            foreach (var y in Y) { s += y + ", "; }
+            s += "]\n";
+            return s;
+        }
 
-    /// <summary>
-    /// Uses the Id of the entity to generate a hash code
-    /// </summary>
-    /// <returns><see cref="int"/> the hash code for this entity</returns>
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Id);
+        /// <summary>
+        /// Calculates the distance from this entity to the origin
+        /// </summary>
+        /// <returns><see cref="double"/> distance from parent sensor</returns>
+        public double DistanceFromParent()
+        {
+            return Math.Sqrt(Math.Pow(this.X[0], 2.0) + Math.Pow(this.Y[0], 2.0));
+        }
+
+        /// <summary>
+        /// Checks if the given object is equal to this entity
+        /// </summary>
+        /// <param name="obj"><see cref="object"/> object to check</param>
+        /// <returns><see cref="bool"/> true if input is entity with the same id</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (obj is not Entity) return false;
+            Entity other = (Entity)obj;
+            return this.Id == other.Id;
+        }
+
+        /// <summary>
+        /// Uses the Id of the entity to generate a hash code
+        /// </summary>
+        /// <returns><see cref="int"/> the hash code for this entity</returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
     }
 }
--- Runtime/Scripts/Models/Payload.cs
+++ Runtime/Scripts/Models/Payload.cs
@@ -2,70 +2,72 @@
 using System.Collections.Generic;
 using Unity.Plastic.Newtonsoft.Json;
 
-/// <summary>
-/// Class for deserializing JSON-objects received from sensor
-/// </summary>
-[Serializable]
-public class Payload
-{
-    // JSON format from sensor:
-    // {"messageType":"position",
-    // "id":"974682A",
-    // "collector_id":"TAC-B",
-    // "collector_serial":"051001572",
-    // "total_detected_objects":1,
-    // "object_list":{
-    // "object0":{
-    // "ID":76,
-    // "speed":0.0,
-    // "X":[-75,-93,0,0,0,0,0,0,0,0],
-    // "Y":[40,51,0,0,0,0,0,0,0,0]}}}
-
+namespace UniTac {
     /// <summary>
-    /// Type of message from publisher.
+    /// Class for deserializing JSON-objects received from sensor
     /// </summary>
-    [JsonProperty("messagetype")]
-    public string MessageType { get; set; } = string.Empty;
-    /// <summary>
-    /// Message Id set by publisher
-    /// </summary>
-    [JsonProperty("id")]
-    public string Id { get; set; } = string.Empty;
-    /// <summary>
-    /// Sensor Id
-    /// </summary>
-    [JsonProperty("collector_id")] 
-    public string CollectorId { get; set; } = string.Empty;
-    /// <summary>
-    /// Sensor serial number
-    /// </summary>
-    [JsonProperty("collector_serial")] 
-    public string CollectorSerial { get; set; } = string.Empty;
-    /// <summary>
-    /// Total number of detected entities.
-    /// </summary>
-    [JsonProperty("total_detected_objects")]
-    public int TotalDetectedEntities;
-    /// <summary>
-    /// List of entities detected by the sensor.
-    /// </summary>
-    [JsonProperty("object_list")]
-    public Dictionary<string,Entity> Entities { get; set; } = new Dictionary<string,Entity>();
-
-    /// <summary>
-    /// Returns a string that represents the current payload.
-    /// </summary>
-    /// <returns>
-    /// <see cref="string" />
-    /// A string that represents the current payload.
-    /// </returns>
-    public override string ToString()
+    [Serializable]
+    public class Payload
     {
-        string s = "Message type: " + MessageType + ", Message id: " + Id + ", Collector id: " + CollectorId + ", Collector serialnumber: " + CollectorSerial + ", Total detected entities: " + TotalDetectedEntities + "\nEntities:\n";
-        foreach (var entity in Entities)
+        // JSON format from sensor:
+        // {"messageType":"position",
+        // "id":"974682A",
+        // "collector_id":"TAC-B",
+        // "collector_serial":"051001572",
+        // "total_detected_objects":1,
+        // "object_list":{
+        // "object0":{
+        // "ID":76,
+        // "speed":0.0,
+        // "X":[-75,-93,0,0,0,0,0,0,0,0],
+        // "Y":[40,51,0,0,0,0,0,0,0,0]}}}
+
+        /// <summary>
+        /// Type of message from publisher.
+        /// </summary>
+        [JsonProperty("messagetype")]
+        public string MessageType { get; set; } = string.Empty;
+        /// <summary>
+        /// Message Id set by publisher
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; } = string.Empty;
+        /// <summary>
+        /// Sensor Id
+        /// </summary>
+        [JsonProperty("collector_id")]
+        public string CollectorId { get; set; } = string.Empty;
+        /// <summary>
+        /// Sensor serial number
+        /// </summary>
+        [JsonProperty("collector_serial")]
+        public string CollectorSerial { get; set; } = string.Empty;
+        /// <summary>
+        /// Total number of detected entities.
+        /// </summary>
+        [JsonProperty("total_detected_objects")]
+        public int TotalDetectedEntities;
+        /// <summary>
+        /// List of entities detected by the sensor.
+        /// </summary>
+        [JsonProperty("object_list")]
+        public Dictionary<string, Entity> Entities { get; set; } = new Dictionary<string, Entity>();
+
+        /// <summary>
+        /// Returns a string that represents the current payload.
+        /// </summary>
+        /// <returns>
+        /// <see cref="string" />
+        /// A string that represents the current payload.
+        /// </returns>
+        public override string ToString()
         {
-            s += entity.Key + ": " + entity.Value.ToString() + "\n";
+            string s = "Message type: " + MessageType + ", Message id: " + Id + ", Collector id: " + CollectorId + ", Collector serialnumber: " + CollectorSerial + ", Total detected entities: " + TotalDetectedEntities + "\nEntities:\n";
+            foreach (var entity in Entities)
+            {
+                s += entity.Key + ": " + entity.Value.ToString() + "\n";
+            }
+            return s;
         }
-        return s;
     }
 }
