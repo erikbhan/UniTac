af31f6dc03f460a5401e08868ee8f9ee581b7407
Gilgard <Hjelljord.alida@gmail.com>
2023-04-13 09:41:03 +0200

    Data packet client

---
+++ Runtime/Scripts/DataPacketClient.cs
@@ -0,0 +1,101 @@
+using MQTTnet.Client;
+using MQTTnet;
+using UnityEngine;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace UniTac
+{
+    /// <summary>
+    /// MQTT-client that saves 5-minute-messages from the sensor to a log file.
+    /// </summary>
+    public class DataPacketClient : MonoBehaviour
+    {
+        /// <summary>
+        /// File path of logfile.
+        /// </summary>
+        public string Path = "";
+        /// <summary>
+        /// Boolean variable toggled in the Inspector GUI; enables logging to the console if true.
+        /// </summary>
+        public bool EnableLogging = false;
+        /// <summary>
+        /// The minimum log level a message from the client needs before it is printed in console.
+        /// </summary>
+        public LogLevel LogLevel = LogLevel.None;
+        private IMqttClient Client;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (Path == "") Path = "./Assets/5-minute-message-log.txt";
+            if (!File.Exists(Path))
+            {
+                File.Create(Path);
+            }
+            if (!File.Exists(Path))
+            {
+                Debug.LogError("Could not create file!");
+            }
+            Client = CreateClient();
+            StartCoroutine(ConnectClient());
+        }
+
+        /// <summary>
+        /// Creates an MQTT client that receives the sensor data from the MQTT server.
+        /// </summary>
+        /// <returns>The client object</returns>
+        IMqttClient CreateClient()
+        {
+            var mqttFactory = new MqttFactory();
+
+            if (EnableLogging)
+            {
+                Logger logger = new(LogLevel);
+                mqttFactory = new MqttFactory(logger);
+            }
+
+            var client = mqttFactory.CreateMqttClient();
+            client.ApplicationMessageReceivedAsync += e => HandleMessage(e);
+            return client;
+        }
+
+        /// <summary>
+        /// Connects the client to the server.
+        /// </summary>
+        /// <returns>awaitable <see cref="Task"/></returns>
+        IEnumerator ConnectClient()
+        {
+            var mqttClientOptions = new MqttClientOptionsBuilder()
+                .WithTcpServer("127.0.0.1")
+                .Build();
+
+            var mqttFactory = new MqttFactory();
+            var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                .WithTopicFilter(
+                    f => { f.WithTopic("smx/device/+/5_min_data"); })
+                .Build();
+
+            yield return new WaitForSeconds(5);
+
+            Client.ConnectAsync(mqttClientOptions, System.Threading.CancellationToken.None);
+            Client.SubscribeAsync(mqttSubscribeOptions, System.Threading.CancellationToken.None);
+        }
+
+        /// <summary>
+        /// A method to process message events from TAC-B sensors.
+        /// </summary>
+        /// <param name="e">The incoming message event</param>
+        /// <returns>awaitable <see cref="Task"/></returns>
+        private Task HandleMessage(MqttApplicationMessageReceivedEventArgs e)
+        {
+            var json = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+            using FileStream fs = new(Path, FileMode.Append, FileAccess.Write);
+            using StreamWriter sw = new(fs);
+            sw.WriteLine(json + ",");
+            return Task.CompletedTask;
+        }
+    }
+}
