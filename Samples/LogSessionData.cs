bb24a145f34ea911ad81095f83489d8705b93e51
Gilgard <Hjelljord.alida@gmail.com>
2023-04-26 09:56:16 +0200

    Samples should be outside namespace

--- Samples/LogSessionData.cs
+++ Samples/LogSessionData.cs
@@ -1,53 +1,51 @@
 using System.IO;
 using Unity.Plastic.Newtonsoft.Json;
 using UnityEngine;
+using UniTac;
 
-namespace UniTac
+/// <summary>
+/// Sample monobehaviour that saves sensor session data to a separate log file.
+/// </summary>
+public class LogSessionData : MonoBehaviour
 {
     /// <summary>
-    /// Sample monobehaviour that saves sensor session data to a separate log file.
+    /// File path of logfile.
     /// </summary>
-    public class LogSessionData : MonoBehaviour
+    public string Path = "";
+    private Sensor Sensor = null;
+
+    /// <summary>
+    /// Gets the sensor and creates a log file if it does not already exist.
+    /// </summary>
+    void Start()
     {
-        /// <summary>
-        /// File path of logfile.
-        /// </summary>
-        public string Path = "";
-        private Sensor Sensor = null;
-
-        /// <summary>
-        /// Gets the sensor and creates a log file if it does not already exist.
-        /// </summary>
-        void Start()
+        Sensor = gameObject.GetComponent<Sensor>();
+        if (!Sensor)
         {
-            Sensor = gameObject.GetComponent<Sensor>();
-            if (!Sensor)
-            {
-                Debug.LogError("Sensor not found");
-                Application.Quit();
-            }
-            Sensor.StatusChangedEvent.AddListener(LogStatus);
-
-            if (Path == "") Path = "./Assets/" + Sensor.Serial + "-Log.txt";
-            if (!File.Exists(Path))
-            {
-                File.Create(Path);
-            }
-            if (!File.Exists(Path))
-            {
-                Debug.LogError("Could not create file!");
-            }
+            Debug.LogError("Sensor not found");
+            Application.Quit();
         }
+        Sensor.StatusChangedEvent.AddListener(LogStatus);
 
-        /// <summary>
-        /// Writes the session data to the file.
-        /// </summary>
-        private void LogStatus()
+        if (Path == "") Path = "./Assets/" + Sensor.Serial + "-Log.txt";
+        if (!File.Exists(Path))
         {
-            var data = JsonConvert.SerializeObject(Sensor.LastSession);
-            using FileStream fs = new(Path, FileMode.Append, FileAccess.Write);
-            using StreamWriter sw = new(fs);
-            sw.WriteLine(data + ",");
+            File.Create(Path);
         }
+        if (!File.Exists(Path))
+        {
+            Debug.LogError("Could not create file!");
+        }
+    }
+
+    /// <summary>
+    /// Writes the session data to the file.
+    /// </summary>
+    private void LogStatus()
+    {
+        var data = JsonConvert.SerializeObject(Sensor.LastSession);
+        using FileStream fs = new(Path, FileMode.Append, FileAccess.Write);
+        using StreamWriter sw = new(fs);
+        sw.WriteLine(data + ",");
     }
 }
