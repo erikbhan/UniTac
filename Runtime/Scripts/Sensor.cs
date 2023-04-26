f19957705e90f6c3d91aec79bb50d1b2107927e0
Gilgard <Hjelljord.alida@gmail.com>
2023-04-26 10:56:04 +0200

    Consistent namespaces under the UniTac domain

--- Runtime/Scripts/Sensor.cs
+++ Runtime/Scripts/Sensor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using UniTac.Models;
 
 namespace UniTac {
     /// <summary>
--- Samples/SpawnOnEntity.cs
+++ Samples/SpawnOnEntity.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UniTac;
+using UniTac.Models;
 
 /// <summary>
 /// Sample monobehaviour that spawns GameObjects that represents any entities detected by the sensor.
