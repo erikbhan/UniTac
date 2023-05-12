c31d6598003e7765a53d15b99a667797c102fa17
Gilgard <Hjelljord.alida@gmail.com>
2023-05-12 09:19:46 +0200

    Updated tests

--- Tests/Runtime/Scripts/Models/PayloadTests.cs
+++ Tests/Runtime/Scripts/Models/PayloadTests.cs
@@ -9,7 +9,7 @@ namespace UniTac.Tests.Models
     public class PayloadTests
     {
         [Test]
-        public void LongPayloadDeserializesCorrectly()
+        public void PayloadDeserializesCorrectly()
         {
             var json = @"{""messagetype"":""position"",""id"":""974682a"",""collector_id"":""tac-b"",""collector_serial"":""051001572"",""total_detected_objects"":1,""object_list"":{""object0"":{""id"":76,""speed"":0.0,""x"":[-75,-93,0,0,0,0,0,0,0,0],""y"":[40,51,0,0,0,0,0,0,0,0]}}}";
 
@@ -28,15 +28,7 @@ namespace UniTac.Tests.Models
         {
             var json = @"{""messagetype"":""position"",""id"":""974682a"",""collector_id"":""tac-b"",""collector_serial"":""051001572"",""total_detected_objects"":1,""object_list"":{""object0"":[""12"",""100"",""215"",""0"", ""5.6""]}}";
 
-            Payload payload;
-            try
-            {
-                payload = JsonConvert.DeserializeObject<Payload>(json) ?? throw new ArgumentNullException();
-
-            } catch (Exception)
-            {
-                payload = new Payload(json);
-            }
+            Payload payload = new Payload(json);
 
             Assert.IsNotNull(payload);
             Assert.AreEqual("position", payload?.MessageType);
@@ -45,6 +37,42 @@ namespace UniTac.Tests.Models
             Assert.AreEqual("051001572", payload?.CollectorSerial);
             Assert.AreEqual(1, payload?.TotalDetectedEntities);
             Assert.IsNotEmpty(payload?.Entities, "Entities not deserialized correctly");
+        }
+
+        [Test]
+        public void PayloadTypeDetection_WithShortPayload_Initializes()
+        {
+            var json = @"{""messagetype"":""position"",""id"":""974682a"",""collector_id"":""tac-b"",""collector_serial"":""051001572"",""total_detected_objects"":1,""object_list"":{""object0"":[""12"",""100"",""215"",""0"", ""5.6""]}}";
+
+            Payload payload;
+            try
+            {
+                payload = JsonConvert.DeserializeObject<Payload>(json) ?? throw new ArgumentNullException();
+            }
+            catch (Exception)
+            {
+                payload = new Payload(json);
+            }
+
+            Assert.IsNotNull(payload.TotalDetectedEntities);
+        }
+
+        [Test]
+        public void PayloadTypeDetection_WithLongPayload_Initializes()
+        {
+            var json = @"{""messagetype"":""position"",""id"":""974682a"",""collector_id"":""tac-b"",""collector_serial"":""051001572"",""total_detected_objects"":1,""object_list"":{""object0"":{""id"":76,""speed"":0.0,""x"":[-75,-93,0,0,0,0,0,0,0,0],""y"":[40,51,0,0,0,0,0,0,0,0]}}}";
+
+            Payload payload;
+            try
+            {
+                payload = JsonConvert.DeserializeObject<Payload>(json) ?? throw new ArgumentNullException();
+            }
+            catch (Exception)
+            {
+                payload = new Payload(json);
+            }
+
+            Assert.IsNotNull(payload.TotalDetectedEntities);
         }
     }
 }
